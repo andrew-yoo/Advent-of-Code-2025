3b29fe3487a5148a13f50cbb54c8a35d4aa9d87a
Andrew Yoo <199436365+andrew-yoo@users.noreply.github.com>
2025-12-02 17:14:05 -0500

    Finished 2025 Day 2

--- day02/part1.py
+++ day02/part1.py
@@ -2,3 +2,28 @@ import sys
 
 sys.stdin = open('input.in', 'r')
 # sys.stdin = open('example.in', 'r')
+
+def read():
+    return [i for i in input().split(',')]
+
+ranges = read()
+
+invalid = []
+
+for range_ in ranges:
+    start, finish = [int(i) for i in range_.split('-')]
+    
+    for i in range(start, finish + 1):
+        
+        id_string = str(i)
+
+        length = len(id_string)
+
+        if length % 2 == 0:
+            half_length = length // 2
+
+            if id_string[:half_length] == id_string[half_length:]:
+                invalid.append(i)
+
+
+print(sum(invalid))
--- day02/part2.py
+++ day02/part2.py
@@ -2,3 +2,32 @@ import sys
 
 sys.stdin = open('input.in', 'r')
 # sys.stdin = open('example.in', 'r')
+
+def read():
+    return [i for i in input().split(',')]
+
+def is_invalid(integer_string: int):
+
+    string = str(integer_string)
+
+    string_length = len(string)
+
+    for repeated_length in range(1, string_length // 2 + 1):
+        if string_length % repeated_length == 0:
+            if string == string[:repeated_length] * (string_length // repeated_length):
+                return True
+            
+    return False
+
+ranges = read()
+
+invalid = []
+
+for range_ in ranges:
+    start, finish = [int(i) for i in range_.split('-')]
+
+    for i in range(start, finish + 1):
+        if is_invalid(i):
+            invalid.append(i)
+        
+print(sum(invalid))
